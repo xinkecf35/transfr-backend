12ef1227a043ffd134052aca55a3c6f5de8364cd
Xinke Chen <xinkecf35@gmail.com>
2018-02-24 20:52:05 -0500

    Added Mongoose/MongoDB models

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  created: {type: Date, default: Date.now},
+  vcardProfiles: [{
+    type: Schema.Types.ObjectId,
+    ref: 'vcardProfile',
+  }],
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ models/vCardProfile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let VCardProfile = new Schema({
+  name: {type: String, required: true},
+  fullName: {type: String, required: true},
+  telephone: String,
+  organization: String,
+  address: String,
+  nickname: String,
+  note: String,
+  birthday: String,
+  impp: String,
+});
+
+module.exports = mongoose.model('VCardProfile', VCardProfile);
