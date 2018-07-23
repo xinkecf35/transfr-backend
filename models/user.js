af45dd972e4a7fd5134270aa76d9887166d51899
Xinke Chen <xinkecf35@gmail.com>
2018-07-22 20:10:42 -0400

    Refactored callbacks to use promises
    This commit largely refactor vcardmanager into using promises to keep internal consistency. Other changes also include additions to 'select' on Schema and queries from inadvertly leaking sensitive information

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ let UserSchema = new Schema({
   username: {type: String, required: true},
   email: {type: String, required: true},
   name: {type: String, required: true},
-  password: {type: String, required: true},
+  password: {type: String, required: true, select: false},
   created: {type: Date, default: Date.now},
   vcards: [{
     type: Schema.Types.ObjectId,
