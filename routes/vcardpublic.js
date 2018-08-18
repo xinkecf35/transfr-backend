87aa5d60db8bb6dd382e203294aabe26819b73c0
Xinke Chen <xinkecf35@gmail.com>
2018-08-18 00:56:15 -0400

    Added public card route
    This commit adds a route to allow any public facing client to access VCard/Profile data provided they have a valid profileId/cardId. Note that id refers to the generated shortcodes and not the ObjectIDs generated by Mongod. In addition, this makes a few changes in other routes that manually set and sent error responses to instead use the error handlers now defined in the app.js

--- routes/vcardpublic.js
+++ routes/vcardpublic.js
@@ -3,8 +3,29 @@ const VCard = require('../models/vcard');
 const sanitize = require('mongo-sanitize');
 const router = new express.Router();
 
-router.get('/:cardId', function(req, res, err) {
+/*
+ * This route allows public access to a specified profile
+ * Route requires a PathParam to indicate what profile to return
+ * Returns a object that is safe for public use;
+ * strips ObjectId and profileId from record
+ */
 
+router.get('/:cardId', function(req, res, next) {
+  let cardId = sanitize(req.params.cardId);
+  let cardQuery = VCard.
+    findOne({profileId: cardId}).
+    select('-_id -profileId -__v').
+    exec();
+  cardQuery.then(function(card) {
+    if (card) {
+      const meta = {sucesss: true};
+      res.json({meta, card});
+    } else {
+      let error = new Error('no such card');
+      error.status = 404;
+      throw error;
+    }
+  }).catch(next);
 });
 
 module.exports = router;
